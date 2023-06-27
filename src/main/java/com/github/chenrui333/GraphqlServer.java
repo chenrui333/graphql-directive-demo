0b95236ef1e1f2e32fa5486b74eed2bce08f634d
Rui Chen <rui@chenrui.dev>
2023-06-27 14:25:24 -0400

    add ServerOptions config

--- src/main/java/com/github/chenrui333/GraphqlServer.java
+++ src/main/java/com/github/chenrui333/GraphqlServer.java
@@ -1,11 +1,16 @@
 package com.github.chenrui333;
 
 import io.jooby.Jooby;
+import io.jooby.ServerOptions;
 
 public class GraphqlServer extends Jooby {
   {
-	get("/", ctx -> "Hello World!");
+    setServerOptions(new ServerOptions()
+        .setPort(8000));
+
+    get("/", ctx -> "Hello World!");
   }
+
 
   public static void main(final String[] args) {
 	runApp(args, GraphqlServer::new);
