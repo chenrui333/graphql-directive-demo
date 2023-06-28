7c5fac99b2aefbb9c532a10b5c980f3a7a525b7d
Rui Chen <rui@chenrui.dev>
2023-06-28 16:38:34 -0400

    some logger support for GraphqlServer
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- src/main/java/com/github/chenrui333/GraphqlServer.java
+++ src/main/java/com/github/chenrui333/GraphqlServer.java
@@ -2,15 +2,25 @@ package com.github.chenrui333;
 
 import io.jooby.Jooby;
 import io.jooby.ServerOptions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class GraphqlServer extends Jooby {
+  private final Logger log = LoggerFactory.getLogger(GraphqlServer.class);
   {
     setServerOptions(new ServerOptions()
         .setPort(8000));
 
     get("/", ctx -> "Hello World!");
     post("/", ctx -> "Hello World Post!");
-    post("/graphql", ctx -> new GraphqlQueryExecutor().execute(ctx.body().value("query")));
+    post("/graphql", ctx ->{
+
+      var query = ctx.body().value("query");
+      log.info("body: {}", ctx.body().value());
+      log.info("query: {}", query);
+      return new GraphqlQueryExecutor().execute(query);
+      }
+    );
   }
 
 
